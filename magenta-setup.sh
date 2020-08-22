cc6d94e0c18f5c90a0933bbbd1b44e9266b3c0bf
Nick Fox-Gieg <nick@fox-gieg.com>
2020-08-22 13:52:48 -0400

    Update magenta-setup.sh

--- magenta-setup.sh
+++ magenta-setup.sh
@@ -3,12 +3,13 @@
 sudo apt-get install build-essential libasound2-dev libjack-dev portaudio19-dev
 python3 -m pip install --upgrade pip
 pip3 install setuptools scikit-build python-rtmidi note_seq colorama 
-pip3 install tensorflow==1.15.0 tensorflow-addons
+pip3 install tensorflow==1.15.0 tensorflow-addons tensorflowjs
 pip3 install tensorflow-probability==0.7.0 absl.py==0.8
-pip3 install magenta 
+pip3 install magenta
 
 cd ..
-git clone https://github.com/tensorflow/magenta.git
+git clone https://github.com/magenta/magenta.git
+git clone https://github.com/magenta/magenta-js.git
 cd magenta
 pip3 install -e .
 

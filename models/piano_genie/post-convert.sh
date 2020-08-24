ab598cceb72fa67619259ac73743c4a2fb6c4e1f
n1ckfg <nick@fox-gieg.com>
2020-08-24 17:27:15 -0400

    shell scripts

--- models/piano_genie/post-convert.sh
+++ models/piano_genie/post-convert.sh
@@ -1,3 +1,4 @@
 conda activate magenta-js
 python checkpoint_converter.py output/checkpoints/v002/model.ckpt-85541 output
 
+ 
--- models/piano_genie/train.sh
+++ models/piano_genie/train.sh
@@ -1,2 +1,2 @@
 conda activate magenta
-python train.py --dataset_fp=/home/nick/GitHub/magenta-bitrate2020/models/nick_midi.tfrecord --train_dir=./output
+python train.py --dataset_fp=/home/nick/GitHub/magenta-bitrate2020/source/nick_midi.tfrecord --train_dir=./output

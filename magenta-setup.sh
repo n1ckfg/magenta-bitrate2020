74e984ce2d5e3fc32f9911cdd3dad21162539906
n1ckfg <nick@fox-gieg.com>
2020-08-24 12:10:25 -0400

    new shell scripts

--- magenta-setup.sh
+++ magenta-setup.sh
@@ -1,15 +1,14 @@
 # https://github.com/magenta/magenta/blob/master/README.md
 
-sudo apt-get install build-essential libasound2-dev libjack-dev portaudio19-dev
-python3 -m pip install --upgrade pip
-pip3 install setuptools scikit-build python-rtmidi note_seq colorama 
-pip3 install tensorflow==1.15.0 tensorflow-addons tensorflowjs
-pip3 install tensorflow-probability==0.7.0 absl.py==0.8
-pip3 install magenta
+# TODO conda
+
+# TODO cuda
+
+bash ./third_party/scripts/magenta-install.sh
+bash ./third_party/scripts/magenta-js-install.sh
 
 cd ..
 git clone https://github.com/magenta/magenta.git
 git clone https://github.com/magenta/magenta-js.git
-cd magenta
-pip3 install -e .
+
 

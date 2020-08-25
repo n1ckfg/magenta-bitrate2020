c118e0065238e6024ae6ae36b81f3769e8b7c9bb
n1ckfg <nick@fox-gieg.com>
2020-08-25 11:12:46 -0400

    shell scripts

--- scripts/melody_rnn-train.sh
+++ scripts/melody_rnn-train.sh
@@ -6,5 +6,4 @@ SCRIPT_PATH=/home/nick/GitHub/magenta/magenta/models/melody_rnn/melody_rnn_train
 
 conda activate magenta
 
-python $SCRIPT_PATH --config=attention_rnn --run_dir=$OUTPUT_DIR --sequence_example_file=$INPUT_DIR --hparams="batch_size=64,rnn_layer_sizes=[64,64]" --num_training_steps=20000
-# --bundle_file=$OUTPUT_DIR/attention_rnn.mag --save_generator_bundle
+python $SCRIPT_PATH --config=attention_rnn --run_dir=$OUTPUT_DIR --sequence_example_file=$INPUT_DIR --hparams="batch_size=64,rnn_layer_sizes=[64,64]" --num_training_steps=20000 --bundle_file=$OUTPUT_DIR/attention_rnn.mag --save_generator_bundle

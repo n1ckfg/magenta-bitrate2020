9e05feb69fb8c2212242a443723a6f4dbffbaadb
Nick Fox-Gieg <nick@fox-gieg.com>
2020-08-27 22:55:43 -0400

    Update helpers.js

--- third_party/piano_genie/js/helpers.js
+++ third_party/piano_genie/js/helpers.js
@@ -5,7 +5,7 @@ const CONSTANTS = {
   WHITE_NOTES_PER_OCTAVE : 7,
   LOWEST_PIANO_KEY_MIDI_NOTE : 21,
   //GENIE_CHECKPOINT : 'https://storage.googleapis.com/magentadata/js/checkpoints/piano_genie/model/epiano/stp_iq_auto_contour_dt_166006',  
-  GENIE_CHECKPOINT : 'model',  
+  GENIE_CHECKPOINT : './js/model',  
 }
 
 /*************************
